3193b80197507c14fb3e0be488732ffcb5500d47
浮生多不虞 <fushengduobuyu@163.com>
2023-10-21 18:06:50 +0800

    init:lab2内容

---
+++ ush-env.c
@@ -0,0 +1,142 @@
+/* ush-env.c */
+/* environment functions for ush */
+
+#include "ush.h"
+#include "ush-env.h"
+
+// this data structure is used to store the 
+// environment assignment information.
+struct varslot  /* symbol table slot */
+{
+    char *name;
+    char *val;
+    BOOLEAN exported;
+}
+sym[MAXVAR];
+
+
+BOOLEAN assign(char **p, char *s)
+{				/* initialize name or value */
+    int size;
+
+    size = strlen(s) + 1;
+    if (*p == NULL) {
+        if ((*p = malloc(size)) == NULL)
+            return (FALSE);
+    }
+    else if ((*p = realloc(*p, size)) == NULL)
+        return (FALSE);
+    strcpy(*p, s);
+    return (TRUE);
+}
+
+//////////////////////////////////////////////////////////
+// You must implement the invoke function 					//
+// do the environment export								       //
+//////////////////////////////////////////////////////////
+BOOLEAN EVexport(char *name)
+{				/* set variable to be exported */    
+    return (TRUE);
+}
+
+
+struct varslot *find(char *name)
+{				/* find symbol table entry */
+
+    int i;
+    struct varslot *v;
+
+    v = NULL;
+    for (i = 0; i < MAXVAR; i++)
+	if (sym[i].name == NULL) {
+	    if (v == NULL)
+		v = &sym[i];
+	}
+	else if (strcmp(sym[i].name, name) == 0) {
+	    v = &sym[i];
+	    break;
+	}
+    return (v);
+}
+
+//////////////////////////////////////////////////////////
+// You must implement the invoke function 					//
+// do the environment set								       //
+//////////////////////////////////////////////////////////
+
+BOOLEAN EVset(char *name, char *val)
+{				/* add name & valude to enviromnemt */
+    return TRUE;
+}
+
+
+BOOLEAN EVinit()
+{				/* initialize symbol table from
+				   environment */
+    int i, namelen;
+    char name[100];
+
+    for (i = 0; environ[i] != NULL; i++) {
+	namelen = strcspn(environ[i], "=");
+	strncpy(name, environ[i], namelen);
+	name[namelen] = '\0';
+	if (!EVset(name, &environ[i][namelen + 1]) || !EVexport(name))
+	    return (FALSE);
+    }
+    return (TRUE);
+}
+
+
+char *EVget(char *name)
+{				/* get value of variable */
+    struct varslot *v;
+
+    if ((v = find(name)) == NULL || v->name == NULL)
+	return (NULL);
+    return (v->val);
+}
+
+void EVprint()
+{				/* printf environment */
+    int i;
+
+    for (i = 0; i < MAXVAR; i++)
+	if (sym[i].name != NULL)
+	    printf("%3s %s=%s\n", sym[i].exported ? "[E]" : "",
+		   sym[i].name, sym[i].val);
+}
+
+//////////////////////////////////////////////////////////
+// You must implement the invoke function 					//
+// do the environment assignment						       //
+//////////////////////////////////////////////////////////
+void asg(int argc, char *argv[])
+{				/* assignment command */
+
+    
+}
+
+//////////////////////////////////////////////////////////
+// You must implement the invoke function 					//
+// do the environment assignment						       //
+//////////////////////////////////////////////////////////
+void set(int argc, char *argv[])
+{				/* set command */
+    
+}
+
+
+void export(int argc, char *argv[])
+{				/* export command */
+    int i;
+
+    if (argc == 1) {
+	set(argc, argv);
+	return;
+    }
+    for (i = 1; i < argc; i++)
+	if (!EVexport(argv[i])) {
+	    printf("Cannot export %s\n", argv[i]);
+	    return;
+	}
+}
---
+++ ush.c
@@ -0,0 +1,187 @@
+#include "ush.h"
+#include "ush-env.h"
+#include "ush-parse.h"
+#include "ush-prt.h"
+#include "ush-sig.h"
+#include "errno.h"
+
+#define MAXARG 20
+#define MAXFNAME 500
+#define MAXWORD  100
+#define BADFD -2
+
+//////////////////////////////////////////////////////////
+// You must implement the invoke function 					//
+// return the process id									     //
+/////////////////////////////////////////////////////////
+static int invoke(int argc, char *argv[], int srcfd, char *srcfile,
+		  int dstfd, char *dstfile, BOOLEAN append,
+		  BOOLEAN bckgrnd)
+			      /* invoke simple command */
+{    
+    return 0;
+}
+
+//////////////////////////////////////////////////////////
+// You must implement the redirect function 					//
+/////////////////////////////////////////////////////////
+static void redirect(int srcfd, char *srcfile, int dstfd, char *dstfile,
+		     BOOLEAN append, BOOLEAN bckgrnd)
+{				/* I/O redirection */
+    
+}
+
+static void waitfor(int pid)
+{				/* wait for child */
+
+    int wpid, status;
+
+    while ((wpid = wait(&status)) != pid && wpid != -1)
+	statusprt(wpid, status);
+    if (wpid == pid)
+	statusprt(0, status);
+}
+//////////////////////////////////////////////////////////
+// You must implement the builtin function 	to do				//
+// the builtin command. 									//
+// Note :: the builtin command should not invoke the linux standard //
+// function.eg, cd should not invoke chdir						//
+// return true if a builtin command ,false other wise				//
+//////////////////////////////////////////////////////////
+
+static BOOLEAN builtin(int argc, char *argv[], int srcfd, int dstfd)
+			      /* do built-in */
+{
+    return TRUE;
+}
+
+static TOKEN command(int *waitpid, BOOLEAN makepipe, int *pipefdp)
+{				/* do simple cmd */
+
+    TOKEN token, term;
+    int argc, srcfd, dstfd, pid, pfd[2];
+    char *argv[MAXARG + 1], srcfile[MAXFNAME], dstfile[MAXFNAME];
+    char word[MAXWORD];
+    BOOLEAN append;   
+
+    argc = 0;
+    srcfd = 0;
+    dstfd = 1;
+    while (1) {
+	switch (token = gettoken(word)) {
+	case T_WORD:
+	    if (argc == MAXARG) {
+		fprintf(stderr, "Too many args\n");
+		break;
+	    }
+	    if ((argv[argc] = malloc(strlen(word) + 1)) == NULL) {
+		fprintf(stderr, "Out of argmemory\n");
+		break;
+	    }
+	    strcpy(argv[argc], word);
+	    argc++;
+	    continue;
+	case T_LT:
+	    if (makepipe) {
+		fprintf(stderr, "Extra<\n");
+		break;
+	    }
+	    if (gettoken(srcfile) != T_WORD) {
+		fprintf(stderr, "Illegal <\n");
+		break;
+	    }
+	    srcfd = BADFD;
+	    continue;
+	case T_GT:
+	    if (dstfd != 1) {
+		fprintf(stderr, "Extra > or >> \n");
+		break;
+	    }
+	    if (gettoken(dstfile) != T_WORD) {
+		fprintf(stderr, "Illegal > or >> \n");
+		break;
+	    }
+	    dstfd = BADFD;
+	    append = FALSE;
+	    continue;
+	case T_GTGT:
+	    if (dstfd != 1) {
+		fprintf(stderr, "Extra > or >>\n");
+		break;
+	    }
+	    if (gettoken(dstfile) != T_WORD) {
+		fprintf(stderr, "Illegal >or >>\n");
+		break;
+	    }
+	    dstfd = BADFD;
+	    append = TRUE;
+	    continue;
+	case T_BAR:
+	case T_AMP:
+	case T_SEMI:
+	case T_NL:
+	    argv[argc] = NULL;
+	    if (token == T_BAR) {
+		if (dstfd != 1) {
+		    fprintf(stderr, "> or >> conflicts with |\n");
+		    break;
+		}
+		term = command(waitpid, TRUE, &dstfd);
+	    }
+	    else
+		term = token;
+
+	    if (makepipe) {
+		if (pipe(pfd) == -1)
+		    syserr("pipe");
+		*pipefdp = pfd[1];
+		srcfd = pfd[0];
+	    }
+	    if (term == T_AMP)
+		pid = invoke(argc, argv, srcfd,
+			     srcfile, dstfd, dstfile, append, TRUE);
+	    else
+		pid = invoke(argc, argv, srcfd, srcfile, dstfd, dstfile,
+			     append, FALSE);
+
+	    if (token != T_BAR)
+		*waitpid = pid;
+	    if (argc == 0 && (token != T_NL || srcfd > 1))
+		fprintf(stderr, "Missing command\n");
+	    while (--argc >= 0)
+		free(argv[argc]);
+	    return (term);
+	case T_EOF:
+	    exit(0);
+	}
+    }
+}
+
+/////////////////////////////////////////////////////////////////
+// you should better not modify this function 						     //
+/////////////////////////////////////////////////////////////////
+int main()
+{				/* real shell */
+    char *prompt;
+    int pid, fd;
+    TOKEN term;
+    void waitfor();
+
+    ignoresig();
+    if (!EVinit())
+	fatal("can't initialize environment");
+    if ((prompt = EVget("PS2")) == NULL)
+	prompt = ">";
+    printf("%s", prompt);
+
+    while (1) {
+	term = command(&pid, FALSE, NULL);
+	if (term != T_AMP && pid != 0)
+	    waitfor(pid);
+	if (term == T_NL)
+	    printf("%s", prompt);
+	for (fd = 3; fd < 20; fd++)
+	    (void) close(fd);	/* ignore error */
+    }
+    return (0);
+}
